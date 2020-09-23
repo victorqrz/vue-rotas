508d981977fc5c3b02948aceb4e5ec8aba42b497
Victor <victoorqrz@gmail.com>
2020-09-23 19:09:38 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    
+    <div class="jumbotron">
+      <h1 class="display-4">Vue-Router</h1>
+      <p class="lead">
+        Adicionando rotas a SPA's no Vue
+      </p>
+    </div>
+
+    <div class="container">
+      <h3 class="font-weight-light">
+        Contatos
+      </h3>
+    </div>
+
+  </div>
+</template>
